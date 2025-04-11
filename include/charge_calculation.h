8c54b8414ae0a0390591d897d6e73382c182d875
yukiyuki <2726165926@qq.com>
2025-04-11 17:10:25 +0800

    .h忘记git了（

--- include/charge_calculation.h
+++ include/charge_calculation.h
@@ -2,7 +2,13 @@
 #define CHARGE_CALCULATION_H
 #include <stdbool.h>
 #include "structure.h"
+//用于计算包裹费用的函数
+//(1-普通用户, 2-VIP用户, 3-企业用户, 4-学生用户, 5-老年用户):
+//(1-文件, 2-生鲜, 3-易碎品, 4-家电, 5-危险品)
+//(1-正常, 2-损坏, 3-违禁品)
+#define base_rate 0.0001
 void calculate_send_package_fees(struct package_s *send_pkg, int customer_type);
 void calculate_receive_package_fees(struct package_r *recv_pkg, int customer_type);
+double calculate_door_to_door(int customer_type,int package_type);
 
 #endif
