776a0b7811940824a3bf5b683c79882e73df4c95
yukiyuki <2726165926@qq.com>
2025-04-19 14:56:50 +0800

    .h

--- include/customer.h
+++ include/customer.h
@@ -6,14 +6,16 @@
 
 // 用户相关功能声明
 
-struct customer *addCustomer(struct customer *head, const char *username, const char *phone_number, const char *password, int customer_type);
+struct customer *addCustomer(struct customer *head);
 void queryCustomer(struct customer *head);
+void displayAllCustomers(struct customer *head);
+struct customer *modifyCustomer(struct customer *head);
 struct customer *deleteCustomer(struct customer *head);
+
 void freeCustomers(struct customer *head);
 int isPhoneNumberDuplicate(struct customer *head, const char *phone_number);
-void displayAllCustomers(struct customer *head);
 void displayMenu_customer();
-struct customer *customersCreating(struct customer *customerList);
+struct customer *customersOperation(struct customer *customerList);
 
 
 #endif // CUSTOMER_H
