476b6383cc586a2fd05fa63115960d4eabaa57a7
数学的屑 <2553198659@qq.com>
2025-04-20 21:01:03 +0800

    改动receved_package.txt

--- src/receivedPackage.c
+++ src/receivedPackage.c
@@ -5,7 +5,7 @@
 #include "structure.h"
 #include "user.h"
 
-int count_day[400] = {0}; // 预计存储365个数据
+int countd[400] = {0}; // 预计存储365个数据
 //每录入一个包裹，就重写huise.txt文件，第i+1行的内容为第i天到达驿站的包裹数
 // 从文件 "huise.txt" 中读取数据并初始化(修改) count_day 数组
 void load_data() {
@@ -15,10 +15,10 @@ void load_data() {
         return;
     }
     for (int i = 0; i < 400; ++i) {
-        count_day[i] = 0; // 初始化数组
+        countd[i] = 0; // 初始化数组
     }
     //读取文件内容到 count_day 数组直到 EOF
-    for (int i = 0; !feof(file) && fscanf(file, "%d", &count_day[i]) == 1; ++i);
+    for (int i = 0; !feof(file) && fscanf(file, "%d", &countd[i]) == 1; ++i);
     fclose(file);
 }
 
@@ -29,7 +29,7 @@ void save_data() {
         return;
     }
     for (int i = 0; i <= 366; i++) {
-        fprintf(file, "%d\n", count_day[i]);
+        fprintf(file, "%d\n", countd[i]);
     }
     fclose(file);
     // printf("数据已保存到文件: %s\n", filename);
@@ -153,13 +153,13 @@ void show_package_r(struct package_r* now, int if_id) {
     }
     printf("5.包裹状态 (1-正常, 2-损坏, 3-违禁品): \n");
     printf("%d\n", (*now).package_status);
+    printf("6.包裹到达时间: \n");
+    printf("%d\n", (*now).day);
 
     if (if_id == 1) {
-        printf("6.包裹序列号: \n");
+        printf("7.包裹序列号: \n");
         printf("%s\n", (*now).package_id);
     }
-    printf("7.包裹到达时间: \n");
-    printf("%d\n", (*now).day);
     return;
 }
 
@@ -437,7 +437,7 @@ void add_received_package(struct package_r *head) {
     // 自动分配包裹序列号
     get_package_r_id(now);
     load_data();
-    count_day[(*now).day]++;
+    countd[(*now).day]++;
     save_data();
     printf("收件包裹添加成功！\n");
     printf("已自动为您分配包裹序列号：%s\n", now->package_id);
@@ -572,21 +572,22 @@ void show_all_packages_r(struct package_r* head) {
     int count = 0;
 
     printf("\n======================= 所有收件包裹信息 ============================\n");
-    printf("%-5s %-18s %-15s %-10s %-10s %-10s %-15s %d\n", 
+    printf("%-5s %-18s %-15s %-10s %-10s %-10s %-15s %-5s\n", 
            "序号", "用户电话", "体积(cm³)", "类型", "到付", "状态", "序列号", "到达时间");
     printf("---------------------------------------------------------------------\n");
 
     while (current != NULL) {
         if (strcmp(current->phone_number, "1") == 0) break;
         count++;
-        printf("%-5d %-15s %-9.2lf %-8s %-14s %-10s %-15s %d\n", 
+        printf("%-5d %-15s %-9.2lf %-8s %-14s %-10s %-15s %5d\n", 
                count, 
                current->phone_number, 
                current->volume, 
                pkgType[current->package_type - 1], // 显示包裹类型的中文描述
                ifcollection[current->ifCollection], // 显示是否到付的中文描述
                pkgStatus[current->package_status - 1], // 显示包裹状态的中文描述
-               current->package_id);
+               current->package_id,
+               current->day);
         current = current->next;
     }
 
